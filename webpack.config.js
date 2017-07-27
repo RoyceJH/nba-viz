144756ffe0d589a47f4adb5b9eeda9e738fc52b3
royce <kim.royce2@gmail.com>
2017-07-27 17:51:59 -0400

    change output dir

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './lib/entry.js',
   output: {
-    filename: './lib/bundle.js',
+    filename: './bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map',
